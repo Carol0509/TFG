d7131e4ef19ba40ef68cd5423aa956c7ea4a6bce
Supley <Supley2@gmail.com>
2026-04-16 13:40:50 +0200

    Juego_solitario

---
+++ Solitario/src/scenes/common.ts
@@ -0,0 +1,20 @@
+export const SCENE_KEYS = {
+  PRELOAD: 'PRELOAD',
+  TITLE: 'TITLE',
+  GAME: 'GAME',
+} as const;
+
+export const ASSET_KEYS = {
+  TITLE: 'TITLE',
+  CLICK_TO_START: 'CLICK_TO_START',
+  CARDS: 'CARDS',
+} as const;
+
+export const CARD_WIDTH = 37;
+export const CARD_HEIGHT = 52;
+
+
+export const AUDIO_KEYS = {
+  CARD_SLIDE: 'CARD_SLIDE',
+  SUCCESS: 'SUCCESS',
+} as const;
